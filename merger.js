c469c783a5cdc6f61859e775ea9954954d395e4e
Dominic Rico Gómez <hello@coderocket.me>
2022-06-06 16:17:34 +0100

    try out changelog building

---
+++ merger.js
@@ -0,0 +1,54 @@
+const { Octokit } = require('@octokit/rest')
+const core = require('@actions/core')
+
+const octokit = new Octokit({
+  auth: core.getInput('token')
+})
+
+const mergeAndCreateChangelog = async () => {
+  const repos = await octokit.rest.repos
+    .listForOrg({
+      org: 'FluffyTal-es',
+      type: 'private',
+      per_page: 1000
+    })
+
+  const changelog_messages = []
+
+  repos.data.map(async (repo) => {
+    const prs = await octokit.rest.search.issuesAndPullRequests({
+      q: `type:pr+repo:FluffyTal-es/${repo.name}`,
+      per_page: 100
+    })
+
+    prs.data.items.map(async (pr) => {
+      /*await github.rest.pulls.createReview({
+        event: "APPROVE",
+        owner: repo.owner,
+        repo: repo.repo,
+        pull_number: pr.number
+      })
+
+      await github.rest.pulls.merge({
+        merge_method: "merge",
+        owner: repo.owner,
+        repo: repo.repo,
+        pull_number: pr.number
+      })*/
+
+      changelog_messages.push(pr.title)
+    })
+  })
+
+  return changelog_messages
+}
+
+(async () => {
+  try {
+    const changelogs = await mergeAndCreateChangelog()
+    core.debug(changelogs)
+    return core.setOutput('changelogs', json.stringify(changelog))
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
